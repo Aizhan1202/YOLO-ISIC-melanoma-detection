b2ce21fb9612efdcb4affdb0c2b53f5076a5bd7c
Aizhan1202 <aizhan-19-99@mail.ru>
2022-10-19 09:15:24 +1000

    Learning rate scheduler

--- modules.py
+++ modules.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, Dropout, LeakyReLU
 from keras.models import Model
 import keras.backend as K
+from tensorflow import keras
 
 
 class Yolo_Reshape(tf.keras.layers.Layer):
@@ -114,4 +115,40 @@ output = block_7(conv)
 
 
 model = Model(inputs, output)
-model.summary()
+
+
+
+class CustomLearningRateScheduler(keras.callbacks.Callback):
+
+    def __init__(self, schedule):
+        super(CustomLearningRateScheduler, self).__init__()
+        self.schedule = schedule
+
+    def on_epoch_begin(self, epoch, logs=None):
+        if not hasattr(self.model.optimizer, "lr"):
+            raise ValueError('Optimizer must have a "lr" attribute.')
+        # Get the current learning rate from model's optimizer.
+        lr = float(tf.keras.backend.get_value(self.model.optimizer.learning_rate))
+        # Call schedule function to get the scheduled learning rate.
+        scheduled_lr = self.schedule(epoch, lr)
+        # Set the value back to the optimizer before this epoch starts
+        tf.keras.backend.set_value(self.model.optimizer.lr, scheduled_lr)
+        print("\nEpoch %05d: Learning rate is %6.4f." % (epoch, scheduled_lr))
+
+
+LR_SCHEDULE = [
+    # (epoch to start, learning rate) tuples
+    (0, 0.01),
+    (20, 0.001),
+    (40, 0.0001),
+]
+
+
+def lr_schedule(epoch, lr):
+    """Helper function to retrieve the scheduled learning rate based on epoch."""
+    if epoch < LR_SCHEDULE[0][0] or epoch > LR_SCHEDULE[-1][0]:
+        return lr
+    for i in range(len(LR_SCHEDULE)):
+        if epoch == LR_SCHEDULE[i][0]:
+            return LR_SCHEDULE[i][1]
+    return lr
